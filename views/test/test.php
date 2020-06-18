f7be039fec10b1776ae0f5fe9d8dabafc481e63c
vs259 <42613172+vs259@users.noreply.github.com>
2020-06-18 15:04:47 +0300

    Add files via upload

--- views/test/test.php
+++ views/test/test.php
@@ -22,28 +22,57 @@
         </nav>
 
         <header>{{ header }} </header>
-    
-        <div class="columns medium-4 rubrics" v-for="(result, index) in rubrics"  v-if="isShowRubrics">
+
+        <div  class="columns medium-4 rubrics"  v-if="isShowRubrics">
           <div class="card">
-            <div class="card-section">
-              <p> {{ result.rubric_title }} </p>
+            <div class="card-section" v-for="(result, index) in rubrics">
+                <p>   
+                    <a href="#" v-on:click="showRubricTree(result.rubric_id)"
+                        v-on:mousedown.right="showRubricNews(result.rubric_id)"
+                        @contextmenu.prevent
+                       >{{ result.rubric_title }}</a>
+                </p>
             </div>
           </div>
+            <br>
+            <p><i>Кликните по рубрике для получения списка вложенных рубрик</i></p>
+            <p><i>Кликните по рубрике правой кнопкой для получения списка новостей выбранной рубрики</i></p>
         </div>
         
-
-        <div class="columns medium-4 news" v-for="(result, index) in news" v-if="isShowNews">
+        <div class="columns medium-4 news" v-if="isShowRubricTree">
           <div class="card">
+            <div class="card-section" v-for="(result, index) in rubrics" >
+                <p>   
+                    <a href="#" v-on:click="showRubricTreeNews(result.rubric_id);">{{ result.rubric_title }}</a>
+                </p>
+            </div>
+          </div>
+            <br>
+            <p><i>Кликните по рубрике для получения списка всех новостей выбранной и вложенных рубрик</i></p>
+        </div>    
+            
+        <div class="columns medium-4 news" v-if="isShowNews">
+          <div class="card"  v-for="(result, index) in news">
             <div class="card-section">
-              <p><b> {{ result.news_title }} </b></p>
+              <p><b>
+                    <a href="#" v-on:click="showRubricsForNews(result.news_id);">{{ result.news_title }}</a>
+                  </b></p>
             </div>
               <br>
             <div class="card-section">
               <p> {{ result.news_text }} </p>
             </div>
           </div>
-              <br>
-              <br>
+            <br>
+            <p><i>Кликните по заголовку новости для получения списка рубрик, к которым принадлежит новость</i></p>
+        </div>
+
+        <div class="columns medium-4 news" v-if="isShowRubricsForNews">
+          <div class="card"  v-for="(result, index) in rubrics">
+            <div class="card-section">
+                <p><small>{{ result.rubric_title }}</small></p>
+            </div>
+          </div>
         </div>
 
     </div>
@@ -61,6 +90,8 @@ let app = new Vue({
         news: {},
         isShowRubrics: false,
         isShowNews: false,
+        isShowRubricTree: false,
+        isShowRubricsForNews: false,
     },
     
     mounted() {
@@ -71,21 +102,63 @@ let app = new Vue({
             this.header = 'РУБРИКИ';
             this.isShowRubrics = true;
             this.isShowNews = false;
+            this.isShowRubricTree = false;
+            this.isShowRubricsForNews = false;
             axios.get("http://dingo.moscow/web/index.php/?r=rubric").then(response => {
-              console.log(response);
             this.rubrics = response.data
             })
          },
         showNews: function() {
             this.header = 'НОВОСТИ';
             this.isShowRubrics = false;
+            this.isShowRubricTree = false;
             this.isShowNews = true;
+            this.isShowRubricsForNews = false;
             axios.get("http://dingo.moscow/web/index.php/?r=news").then(response => {
-              console.log(response);
             this.news = response.data
             })
         },
-    },
+        showRubricTree: function(index){
+            this.header = 'Рубрика + дочернии, с учетом произвольного уровня вложенности';
+            this.isShowRubrics = false;
+            this.isShowNews = false;
+            this.isShowRubricTree = true;
+            this.isShowRubricsForNews = false;
+            axios.get("http://dingo.moscow/web/index.php/?r=rubric&rubric_id=" + index).then(response => {
+            this.rubrics = response.data
+            })
+        },
+        showRubricNews: function(index){
+            this.header = 'НОВОСТИ';
+            this.isShowRubrics = false;
+            this.isShowRubricTree = false;
+            this.isShowNews = true;
+            this.isShowRubricsForNews = false;
+            axios.get("http://dingo.moscow/web/index.php/?r=news&rubric_id=" + index).then(response => {
+            this.news = response.data
+            })
+        },
+        showRubricTreeNews: function(index){
+            this.header = 'НОВОСТИ';
+            this.isShowRubrics = false;
+            this.isShowRubricTree = false;
+            this.isShowNews = true;
+            this.isShowRubricsForNews = false;
+            axios.get("http://dingo.moscow/web/index.php/?r=news&parent_id=" + index).then(response => {
+            this.news = response.data
+            })
+        },
+        showRubricsForNews: function(index){
+            this.header = 'НОВОСТИ';
+            this.isShowRubrics = false;
+            this.isShowRubricTree = false;
+            this.isShowNews = true;
+            this.isShowRubricsForNews = true;
+            axios.get("http://dingo.moscow/web/index.php/?r=news&id=" + index).then(response => {
+            this.rubrics = response.data
+            })
+        },
+     },
 });
 
 
