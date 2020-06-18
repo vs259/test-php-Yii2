1139b7d19ba462ef9d1bb8c358ceb7d72c0b7dd7
vs259 <42613172+vs259@users.noreply.github.com>
2020-06-18 09:52:46 +0300

    Add files via upload

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -1,23 +1,55 @@
 <?php
 namespace app\controllers;
 
+use Yii;
 use yii\rest\ActiveController;
+use yii\db\ActiveQuery;
+
+use app\models\News;
 
 class NewsController extends ActiveController
 {
     public $modelClass = 'app\models\News';
     
+    /* Declare actions supported by APIs (Added in api/modules/v1/components/controller.php too) */
+    public function actions(){
+        $actions = parent::actions();
+        unset($actions['create']);
+        unset($actions['update']);
+        unset($actions['delete']);
+        unset($actions['view']);
+        unset($actions['index']);
+        return $actions;
+    }
+
+    /* Declare methods supported by APIs */
+    protected function verbs(){
+        return [
+            'index'=>['GET'],
+        ];
+    }
+    
     public function actionIndex()
     {
-        $modelClass = $this->modelClass;        
-        $query = $modelClass::findOne([
-        'news_id' => Yii::$app->request->get('news_id'),
-    ]);
-        return new ActiveDataProvider([
-            'query' => $query,
-        ]);
+        $rubric_id = Yii::$app->request->get('rubric_id');
+        if(isset($rubric_id)){
 
-    }
+            $news = (new \yii\db\Query())
+                    ->select('news.*')
+                    ->from('news_rubric')
+                    ->innerJoin('news', 'news.news_id = news_rubric.news_id')
+                    ->where(['news_rubric.rubric_id' => $rubric_id])
+                    ->all();
+            return $news;
+        }
+        
+        
+        $news_id = Yii::$app->request->get('news_id');
+        if(isset($news_id))
+            return News::findOne($news_id);  
+        else
+            return News::find()->all();        
+        }
     
 }
 
