03aa238f88afaaec75009de9dd7acee4168519ff
vs259 <42613172+vs259@users.noreply.github.com>
2020-06-18 14:38:40 +0300

    Add files via upload

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 use Yii;
 use yii\rest\ActiveController;
 use yii\db\ActiveQuery;
+use \yii\db\Query;
 
 use app\models\News;
 
@@ -34,7 +35,7 @@ class NewsController extends ActiveController
         $rubric_id = Yii::$app->request->get('rubric_id');
         if(isset($rubric_id)){
 
-            $news = (new \yii\db\Query())
+            $news = (new Query())
                     ->select('news.*')
                     ->from('news_rubric')
                     ->innerJoin('news', 'news.news_id = news_rubric.news_id')
@@ -43,6 +44,43 @@ class NewsController extends ActiveController
             return $news;
         }
         
+        $parent_id = Yii::$app->request->get('parent_id');
+        if(isset($parent_id)){
+            $news = Yii::$app->db->createCommand(
+                    "
+                    select distinct n.* 
+                    from news_rubric as nr 
+                            inner join news as n
+                                    on n.news_id = nr.news_id
+                    where nr.rubric_id in (
+                    select  r.rubric_id
+                    from rubric as r
+                    where r.rubric_id = :id
+                    union 
+                    select  rubric_id
+                    from    (select * from rubric
+                    order by parent_id, rubric_id) as rubric_sorted,
+                    (select @pv := :id) initialisation
+                    where   find_in_set(parent_id, @pv)
+                    and     length(@pv := concat(@pv, ',', rubric_id)))                    
+                    "
+                    , ['id' => $parent_id])
+                    ->queryAll();
+            return $news;
+        }
+        
+        
+        $id = Yii::$app->request->get('id');
+        if(isset($id)){
+            $news = (new Query())
+                    ->select('rubric.*')
+                    ->from('news')
+                    ->rightJoin('news_rubric', 'news.news_id = news_rubric.news_id')
+                    ->innerJoin('rubric', 'rubric.rubric_id = news_rubric.rubric_id')
+                    ->where(['news.news_id' => $id])
+                    ->all();
+            return $news;
+        }
         
         $news_id = Yii::$app->request->get('news_id');
         if(isset($news_id))
